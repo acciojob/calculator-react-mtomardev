474874aefb8a400f4bb505ca07f313b5e1b00022
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-06 16:11:42 +0530

    calculator

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,69 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, {useState} from "react";
+import "./style.css"
 
+
+const App = () =>{
+    let [calculate, setCalculate] = useState("")
+
+    function handlecalculations(e){
+        setCalculate(calculate + e.target.innerText)
+        console.log(e.target.innerText)
+    }
+
+    function result (){
+        try{
+        setCalculate(eval(calculate))
+    }
+    catch(err){
+        setCalculate("Invalid Input")
+    }
+    }
+
+    function clear(){
+        setCalculate("")
+    }
+    
+
+    return(
+        <div>
+            <div className="calculatorDiv">
+            <input className="inputDiv" type="text" value={calculate}/>
+            
+            <table>
+                <tr>
+                    <td className="btn" onClick={clear}>C</td>
+                    <td className="btn" onClick={handlecalculations}>/</td>
+                    <td className="btn" onClick={handlecalculations}>*</td>
+                    <td className="btn" onClick={handlecalculations}>-</td>
+                </tr>
+                <tr>
+                    <td className="btn" onClick={handlecalculations}>7</td>
+                    <td className="btn" onClick={handlecalculations}>8</td>
+                    <td className="btn" onClick={handlecalculations}>9</td>
+                    <td rowSpan={2} className="btn" onClick={handlecalculations}>+</td>
+                </tr>
+                <tr>
+                    <td className="btn" onClick={handlecalculations}>4</td>
+                    <td className="btn" onClick={handlecalculations}>5</td>
+                    <td className="btn" onClick={handlecalculations}>6</td>
+                </tr>
+                <tr>
+                    <td className="btn" onClick={handlecalculations}>1</td>
+                    <td className="btn" onClick={handlecalculations}>2</td>
+                    <td className="btn" onClick={handlecalculations}>3</td>
+                    <td rowSpan={2} className="btn" onClick={result}>=</td>
+                </tr>
+                <tr>
+                    <td className="btn" onClick={handlecalculations}>0</td>
+                    <td className="btn" onClick={handlecalculations}>.</td>
+                    <td className="btn"></td>
+                </tr>
+            </table>
+            
+        </div>
+
+        </div>
+    )
+}
+
+export default App;
